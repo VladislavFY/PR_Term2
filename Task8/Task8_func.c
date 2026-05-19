4d8ca2afd7b99b67bd6baf3f33c69f601336da3b
VladislavFY <vladislavfrolov2008@gmail.com>
2026-05-19 11:26:09 +0300

    Update Task8_func.c

--- Task8/Task8_func.c
+++ Task8/Task8_func.c
@@ -1,301 +1,350 @@
 #include "prototypes8.h"
 
-void free_students(Student *students) {
-    if (students == NULL) {
-        return;
-    }
-    free(students);
-}
-int generate_students(const char *surname_file_name, Student **students, int *count, int min_group, int max_group, int min_school, int max_school) {
-    FILE *surname_file;
-    char surnames[1000][NAME_LEN];
-    int surname_count = 0;
-    int i;
-
-    if (surname_file_name == NULL || students == NULL || count == NULL) {
-        return -1;
-    }
-
-    if (*count <= 0) {
-        return -2;
-    }
-
-    if (min_group > max_group || min_school > max_school) {
-        return -3;
-    }
-
-    surname_file = fopen(surname_file_name, "r");
-    if (surname_file == NULL) {
-        return -4;
-    }
-
-    while (surname_count < 1000 && fscanf(surname_file, "%127s", surnames[surname_count]) == 1) {
-        surname_count++;
-    }
-
-    fclose(surname_file);
-
-    if (surname_count == 0) {
-        return -5;
-    }
-
-    *students = (Student *)malloc((*count) * sizeof(Student));
-    if (*students == NULL) {
-        return -6;
-    }
-
-    srand((unsigned int)time(NULL));
-
-    for (i = 0; i < *count; ++i) {
-        int surname_index;
-        int group;
-        int school;
-
-        surname_index = rand() % surname_count;
-        group = min_group + rand() % (max_group - min_group + 1);
-        school = min_school + rand() % (max_school - min_school + 1);
-
-        strcpy((*students)[i].Name, surnames[surname_index]);
-        (*students)[i].Group = group;
-        (*students)[i].School = school;
-    }
-
-    return 0;
+void init_database(StudentData *database) {
+ if (database == NULL) {
+  return;
+ }
+ database->students = NULL;
+ database->count = 0;
 }
 
-int read_students(FILE *f, Student **students, int *count) {
-    Student temp;
-    Student *new_students;
-    int capacity = 10;
-
-    if (f == NULL || students == NULL || count == NULL) {
-        return -1;
-    }
-
-    *count = 0;
-    *students = (Student *)malloc(capacity * sizeof(Student));
-    if (*students == NULL) {
-        return -2;
-    }
-
-    while (fscanf(f, "%127s %d %d", temp.Name, &temp.Group, &temp.School) == 3) {
-        if (*count >= capacity) {
-            capacity *= 2;
-            new_students = (Student *)realloc(*students, capacity * sizeof(Student));
-            if (new_students == NULL) {
-                free(*students);
-                *students = NULL;
-                *count = 0;
-                return -2;
-            }
-            *students = new_students;
-        }
-
-        (*students)[*count] = temp;
-        (*count)++;
-    }
-
-    return 0;
+void free_students(StudentData *database) {
+ if (database == NULL) {
+  return;
+ }
+ if (database->students == NULL) {
+  return;
+ }
+ free(database->students);
+ database->students = NULL;
+ database->count = 0;
 }
 
-int write_students(FILE *f, Student *students, int count) {
-    int i;
-
-    if (f == NULL || students == NULL) {
-        return -1;
-    }
-
-    for (i = 0; i < count; ++i) {
-        fprintf(f, "%s %d %d", students[i].Name, students[i].Group, students[i].School);
-        if (i + 1 < count) {
-            fprintf(f, "\n");
-        }
-    }
-
-    return 0;
+int copy_database(StudentData *from, StudentData *to) {
+ int i;
+ if (from == NULL || to == NULL) {
+  return -1;
+ }
+ init_database(to);
+ if (from->students == NULL || from->count <= 0) {
+  return 0;
+ }
+ to->students = (Student *)malloc(from->count * sizeof(Student));
+ if (to->students == NULL) {
+  return -2;
+ }
+ to->count = from->count;
+ for (i = 0; i < from->count; ++i) {
+  to->students[i] = from->students[i];
+ }
+ return 0;
 }
 
-int has_schoolmate_in_group(Student *students, int count, int index, int group) {
-    int i;
+int generate_students(const char *surname_file_name, StudentData *database, int count, int min_group, int max_group, int min_school, int max_school) {
+ FILE *surname_file;
+ char surnames[1000][NAME_LEN];
+ int surname_count = 0;
+ int i;
 
-    if (students == NULL || index < 0 || index >= count) {
-        return 0;
-    }
+ if (surname_file_name == NULL || database == NULL) {
+  return -1;
+ }
+ if (count <= 0) {
+  return -2;
+ }
+ if (min_group > max_group || min_school > max_school) {
+  return -3;
+ }
 
-    for (i = 0; i < count; ++i) {
-        if (i == index) {
-            continue;
-        }
+ surname_file = fopen(surname_file_name, "r");
+ if (surname_file == NULL) {
+  return -4;
+ }
 
-        if (students[i].Group == group && students[i].School == students[index].School) {
-            return 1;
-        }
-    }
+ while (surname_count < 1000 && fscanf(surname_file, "%127s", surnames[surname_count]) == 1) {
+  surname_count++;
+ }
+ fclose(surname_file);
 
-    return 0;
+ if (surname_count == 0) {
+  return -5;
+ }
+
+ database->students = (Student *)malloc(count * sizeof(Student));
+ if (database->students == NULL) {
+  return -6;
+ }
+ database->count = count;
+
+ srand((unsigned int)time(NULL));
+ for (i = 0; i < database->count; ++i) {
+  int surname_index;
+  int group;
+  int school;
+  surname_index = rand() % surname_count;
+  group = min_group + rand() % (max_group - min_group + 1);
+  school = min_school + rand() % (max_school - min_school + 1);
+  strcpy(database->students[i].Name, surnames[surname_index]);
+  database->students[i].Group = group;
+  database->students[i].School = school;
+ }
+ return 0;
 }
 
-int group_has_school(Student *students, int count, int group, int school, int skip_index) {
-    int i;
+int read_students(FILE *f, StudentData *database) {
+ Student temp;
+ Student *new_students;
+ int capacity = 10;
 
-    if (students == NULL) {
-        return 0;
-    }
+ if (f == NULL || database == NULL) {
+  return -1;
+ }
 
-    for (i = 0; i < count; ++i) {
-        if (i == skip_index) {
-            continue;
-        }
+ database->count = 0;
+ database->students = (Student *)malloc(capacity * sizeof(Student));
+ if (database->students == NULL) {
+  return -2;
+ }
 
-        if (students[i].Group == group && students[i].School == school) {
-            return 1;
-        }
-    }
-
-    return 0;
+ while (fscanf(f, "%127s %d %d", temp.Name, &temp.Group, &temp.School) == 3) {
+  if (database->count >= capacity) {
+   capacity *= 2;
+   new_students = (Student *)realloc(database->students, capacity * sizeof(Student));
+   if (new_students == NULL) {
+    free_students(database);
+    return -2;
+   }
+   database->students = new_students;
+  }
+  database->students[database->count] = temp;
+  database->count++;
+ }
+ return 0;
 }
 
-void redistribute_students(Student *students, int count) {
-    int i;
-    int j;
+int write_students(FILE *f, StudentData *database) {
+ int i;
+ if (f == NULL || database == NULL || database->students == NULL) {
+  return -1;
+ }
+ for (i = 0; i < database->count; ++i) {
+  fprintf(f, "%s %d %d", database->students[i].Name, database->students[i].Group, database->students[i].School);
+  if (i + 1 < database->count) {
+   fprintf(f, "\n");
+  }
+ }
+ return 0;
+}
 
-    if (students == NULL || count <= 1) {
-        return;
-    }
+int compare_students(const void *a, const void *b) {
+ const Student *student_a = (const Student *)a;
+ const Student *student_b = (const Student *)b;
+ int name_compare;
 
-    for (i = 0; i < count; ++i) {
-        int group_a = students[i].Group;
-        int school_a = students[i].School;
+ if (student_a->Group != student_b->Group) {
+  return student_a->Group - student_b->Group;
+ }
+ if (student_a->School != student_b->School) {
+  return student_a->School - student_b->School;
+ }
+ name_compare = strcmp(student_a->Name, student_b->Name);
+ return name_compare;
+}
 
-        if (!has_schoolmate_in_group(students, count, i, group_a)) {
-            continue;
-        }
+void sort_students(StudentData *database) {
+ if (database == NULL || database->students == NULL || database->count <= 1) {
+  return;
+ }
+ qsort(database->students, database->count, sizeof(Student), compare_students);
+}
 
-        for (j = 0; j < count; ++j) {
-            int group_b;
-            int school_b;
-            int temp_group;
+int has_schoolmate_in_group(StudentData *database, int index, int group) {
+ int i;
+ if (database == NULL || database->students == NULL || index < 0 || index >= database->count) {
+  return 0;
+ }
+ for (i = 0; i < database->count; ++i) {
+  if (i == index) {
+   continue;
+  }
+  if (database->students[i].Group == group && database->students[i].School == database->students[index].School) {
+   return 1;
+  }
+ }
+ return 0;
+}
 
-            if (i == j) {
-                continue;
-            }
+int group_has_school(StudentData *database, int group, int school, int skip_index) {
+ int i;
+ if (database == NULL || database->students == NULL) {
+  return 0;
+ }
+ for (i = 0; i < database->count; ++i) {
+  if (i == skip_index) {
+   continue;
+  }
+  if (database->students[i].Group == group && database->students[i].School == school) {
+   return 1;
+  }
+ }
+ return 0;
+}
 
-            group_b = students[j].Group;
-            school_b = students[j].School;
+int count_problem_students(StudentData *database) {
+ int i;
+ int count = 0;
+ if (database == NULL || database->students == NULL) {
+  return 0;
+ }
+ for (i = 0; i < database->count; ++i) {
+  if (has_schoolmate_in_group(database, i, database->students[i].Group)) {
+   count++;
+  }
+ }
+ return count;
+}
 
-            if (group_b == group_a) {
-                continue;
-            }
-
-            if (group_has_school(students, count, group_a, school_b, i)) {
-                continue;
-            }
-            
-            if (group_has_school(students, count, group_b, school_a, j)) {
-                continue;
-            }
-            
-            temp_group = students[i].Group;
-            students[i].Group = students[j].Group;
-            students[j].Group = temp_group;
-
-            break;
-        }
-    }
+void redistribute_students(StudentData *database) {
+ int i;
+ int j;
+ if (database == NULL || database->students == NULL || database->count <= 1) {
+  return;
+ }
+ for (i = 0; i < database->count; ++i) {
+  int group_a = database->students[i].Group;
+  int school_a = database->students[i].School;
+  if (!has_schoolmate_in_group(database, i, group_a)) {
+   continue;
+  }
+  for (j = 0; j < database->count; ++j) {
+   int group_b;
+   int school_b;
+   int temp_group;
+   if (i == j) {
+    continue;
+   }
+   group_b = database->students[j].Group;
+   school_b = database->students[j].School;
+   if (group_b == group_a) {
+    continue;
+   }
+   if (group_has_school(database, group_a, school_b, i)) {
+    continue;
+   }
+   if (group_has_school(database, group_b, school_a, j)) {
+    continue;
+   }
+   temp_group = database->students[i].Group;
+   database->students[i].Group = database->students[j].Group;
+   database->students[j].Group = temp_group;
+   break;
+  }
+ }
+ sort_students(database);
 }
 
 int main_function(void) {
-    FILE *f_in;
-    FILE *f_out;
-    Student *students = NULL;
-    int count = 0;
-    int status;
-    int choice;
-    int i; 
-    
-    printf("Choose input mode:\n");
-    printf("1 - Read students from data.dat\n");
-    printf("2 - Generate students automatically\n");
-    printf("Your choice: ");
+ FILE *f_in;
+ FILE *f_out;
+ StudentData database;
+ StudentData original_database;
+ int status;
+ int choice;
+ int problems_before = 0;
+ int problems_after = 0;
 
-    if (scanf("%d", &choice) != 1) {
-        return -1;
-    }
+ init_database(&database);
+ init_database(&original_database);
 
-    if (choice == 1) {
-        f_in = fopen("data.dat", "r");
-        if (f_in == NULL) {
-            return -2;
-        }
+ printf("Choose input mode:\n");
+ printf("1 - Read students from data.dat\n");
+ printf("2 - Generate students automatically\n");
+ printf("Your choice: ");
+ if (scanf("%d", &choice) != 1) {
+  return -1;
+ }
 
-        status = read_students(f_in, &students, &count);
-        fclose(f_in);
+ if (choice == 1) {
+  f_in = fopen("data.dat", "r");
+  if (f_in == NULL) {
+   return -2;
+  }
+  status = read_students(f_in, &database);
+  fclose(f_in);
+  if (status != 0) {
+   free_students(&database);
+   return -3;
+  }
+ } else if (choice == 2) {
+  int count;
+  int min_group;
+  int max_group;
+  int min_school;
+  int max_school;
 
-        if (status != 0) {
-            free_students(students);
-            return -3;
-        }
-    } else if (choice == 2) {
-        int min_group;
-        int max_group;
-        int min_school;
-        int max_school;
+  printf("Enter number of students: ");
+  if (scanf("%d", &count) != 1) {
+   return -4;
+  }
+  printf("Enter minimum group number: ");
+  if (scanf("%d", &min_group) != 1) {
+   return -5;
+  }
+  printf("Enter maximum group number: ");
+  if (scanf("%d", &max_group) != 1) {
+   return -6;
+  }
+  printf("Enter minimum school number: ");
+  if (scanf("%d", &min_school) != 1) {
+   return -7;
+  }
+  printf("Enter maximum school number: ");
+  if (scanf("%d", &max_school) != 1) {
+   return -8;
+  }
 
-        printf("Enter number of students: ");
-        if (scanf("%d", &count) != 1) {
-            return -4;
-        }
+  status = generate_students("surnames.txt", &database, count, min_group, max_group, min_school, max_school);
+  if (status != 0) {
+   return -9;
+  }
+ } else {
+  return -10;
+ }
 
-        printf("Enter minimum group number: ");
-        if (scanf("%d", &min_group) != 1) {
-            return -5;
-        }
+ sort_students(&database);
+ problems_before = count_problem_students(&database);
 
-        printf("Enter maximum group number: ");
-        if (scanf("%d", &max_group) != 1) {
-            return -6;
-        }
+ if (choice == 2) {
+  status = copy_database(&database, &original_database);
+  if (status != 0) {
+   free_students(&database);
+   return -11;
+  }
+ }
 
-        printf("Enter minimum school number: ");
-        if (scanf("%d", &min_school) != 1) {
-            return -7;
-        }
+ redistribute_students(&database);
+ problems_after = count_problem_students(&database);
 
-        printf("Enter maximum school number: ");
-        if (scanf("%d", &max_school) != 1) {
-            return -8;
-        }
+ f_out = fopen("data.res", "w");
+ if (f_out == NULL) {
+  free_students(&database);
+  free_students(&original_database);
+  return -12;
+ }
 
-        status = generate_students("surnames.txt", &students, &count, min_group, max_group, min_school, max_school);
+ if (choice == 2) {
+  fprintf(f_out, "Original random list of students:\n");
+  write_students(f_out, &original_database);
+  fprintf(f_out, "\nProblem cases before redistribute: %d\n\n", problems_before);
+  fprintf(f_out, "Changed list of students:\n");
+  write_students(f_out, &database);
+  fprintf(f_out, "\nProblem cases after redistribute: %d\n", problems_after);
+ } else {
+  write_students(f_out, &database);
+ }
 
-        if (status != 0) {
-            return -9;
-        }
-    } else {
-        return -10;
-    }
-    
-    if(choice==2){
-        printf("Original random list of students:\n");
-        for (i = 0; i < count; ++i) {
-            printf("%s %d %d", students[i].Name, students[i].Group, students[i].School);
-            if (i + 1 < count) {
-            printf("\n");
-            }
-        }
-    }
-    redistribute_students(students, count);
-
-    f_out = fopen("data.res", "w");
-    if (f_out == NULL) {
-        free_students(students);
-        return -11;
-    }
-
-    write_students(f_out, students, count);
-    fclose(f_out);
-
-    free_students(students);
-
-    return 0;
+ fclose(f_out);
+ free_students(&database);
+ free_students(&original_database);
+ return 0;
 }
