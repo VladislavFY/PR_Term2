86a80fbb72399680718a03d7b82c11aab448a26d
VladislavFY <vladislavfrolov2008@gmail.com>
2026-05-21 18:20:30 +0300

    Update Task8_main.c

--- Task8/Task8_main.c
+++ Task8/Task8_main.c
@@ -1,6 +1,11 @@
 #include "prototypes8.h"
 
-int main(){
-    main_function();
-    return 0;
+int main(void) {
+    int status = main_function();
+
+    if (status != 0) {
+        printf("Program finished with error: %d\n", status);
+    }
+
+    return status;
 }
